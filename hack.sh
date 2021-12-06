1aff51f0641254222a6029228b1c2c312cfe2110
zantiago el-scrikkidyes <73414733+SANTIAGOVELASCOJOSEMANUEL@users.noreply.github.com>
2021-12-06 00:22:44 -0600

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,391 @@
+#!/bin/bash
+clear
+sleep 2
+echo -e "\e[31m                   W\e[0m"
+echo -e "\e[1;31m                  WWW\e[0m"
+echo -e "\e[1;30m                  WWW\e[0m"
+echo -e "\e[34m                 WWWWW\e[0m"
+echo -e "\e[1;34m           W     WWWWW     W\e[0m"
+echo -e "\e[32m           WWW   WWWWW   WWW\e[0m"
+echo -e "\e[1;32m            WWW  WWWWW  WWW\e[0m"
+echo -e "\e[36m             WWW  WWW  WWW\e[0m"
+echo -e "\e[1;36m              WWW WWW WWW\e[0m"
+echo -e "\e[35m                WWWWWWW\e[0m"
+echo -e "\e[1;35m             WWWW  |  WWWW\e[0m"
+echo -e "\e[33m                   |\e[0m"
+echo -e "\e[1;33m                   |\e[0m"
+echo -e "\e[1;32m     🌹🌹🌹 zantiago El-Scrikkidyes🌹🌹🌹\e[0m"
+echo -e "\e[1;32m 🌹🌹 COMANDOS TERMUX SQL LINUX GOOGLE Y MÁS🌹🌹\e[0m"
+echo " "
+
+sleep 3
+echo -e "\e[1;32m  HACKEANDO LA NASA\e[0m "
+
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 1% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 2% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 3% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 4% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 5% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 6% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 7% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 8% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 9% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 10% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 11% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 12% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 13% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 14% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 15% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 16% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 17% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 18% \e[0m"
+
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 19% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 20% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 21% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 22% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 23% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 24% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 25% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 26% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 27% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 28% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 29% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 30% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 31% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 32% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 33% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 34% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 35% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 36% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 37% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 38% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 39% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 40% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 41% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 42% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 43% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 44% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 45% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 46% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 47% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 48% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 49% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 50% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 51% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 52% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 53% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 54% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 55% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 56% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 57% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 58% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 59% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 60% \e[0m"
+
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 61% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 62% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 63% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 64% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 65% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 66% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 67% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 68% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 69% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 70% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 71% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 72% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 73% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 74% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 75% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 76% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 77% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 78% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 79% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 80% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 81% \e[0m"
+sleep 1
+echo " "
+echo -e " \e[1;35m cargando 82% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 83% \e[0m"
+echo " "
+sleep 1
+echo -e " \e[1;35m cargando 84% \e[0m"
+echo " "
+sleep 1
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m enviando ubicación\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m descargando malware\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m instalando malware\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m borrando storage\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m borrando storage\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ##\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ###\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ####\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #####\e[0m"   
+echo " "
+sleep 0.9
+echo -e "\e[31m ######\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #######\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ########\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #########\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ##########\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ###########\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ############\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #############\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ##############\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ###############\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ################\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #################\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ##################\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ###################\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ####################\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m #####################\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m storage borrado\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m capturando contraseña\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m enviando contraseña\e[0m"
+echo " "
+sleep 0.9
+echo -e "\e[31m ALERTA\e[0m"
+echo " "
+sleep 5
+echo -e "\e[31m USTED FUE TROLEADO POR ZANTIAGO EL-SCRIKKIDYES 😏\e[0m"
